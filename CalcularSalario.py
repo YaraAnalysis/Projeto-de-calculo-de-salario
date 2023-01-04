d998cdbd7af6e19d1a07d71c9c8b331209d4dcfd
YaraAnalysis <114318856+YaraAnalysis@users.noreply.github.com>
2023-01-04 19:25:23 -0300

    variáveis e primeiros prints

--- CalcularSalario.py
+++ CalcularSalario.py
@@ -2,3 +2,5 @@ valor_hora = float(input('Informe o valor da sua hora trabalhada em reais: '))
 qtd_horas_mes = float(input('Informe o quantidade de horas trabalhadas no mês: '))
 salario_bruto = qtd_horas_mes * valor_hora
 
+print('Salário do mês: R$',{salario_bruto})
+print('Descontos aplicados: ')
