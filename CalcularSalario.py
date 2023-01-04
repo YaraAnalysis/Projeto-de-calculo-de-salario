923d785d91ce981f03a31ad1f0b2cebf16b37114
YaraAnalysis <114318856+YaraAnalysis@users.noreply.github.com>
2023-01-04 00:14:17 -0300

    Create CalcularSalario.py

---
+++ CalcularSalario.py
@@ -0,0 +1,4 @@
+valor_hora = float(input('Informe o valor da sua hora trabalhada em reais: '))
+qtd_horas_mes = float(input('Informe o quantidade de horas trabalhadas no mês: '))
+salario_bruto = qtd_horas_mes * valor_hora
+
