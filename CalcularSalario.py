ba42ad5b625eec1c3613017c695bf63d7c3d69b7
YaraAnalysis <114318856+YaraAnalysis@users.noreply.github.com>
2023-01-05 18:02:24 -0300

    input dos funcionários e salários
    input dos funcionários e salários

--- CalcularSalario.py
+++ CalcularSalario.py
@@ -1,6 +1,25 @@
-valor_hora = float(input('Informe o valor da sua hora trabalhada em reais: '))
-qtd_horas_mes = float(input('Informe o quantidade de horas trabalhadas no mês: '))
-salario_bruto = qtd_horas_mes * valor_hora
+#valor_hora = float(input('Informe o valor da sua hora trabalhada em reais: '))
+#qtd_horas_mes = float(input('Informe o quantidade de horas trabalhadas no mês: '))
+#salario_bruto = qtd_horas_mes * valor_hora
 
-print('Salário do mês: R$',{salario_bruto})
-print('Descontos aplicados: ')
+#print('Salário do mês: R$',{salario_bruto})
+#print('Descontos aplicados: ')
+
+funcionarios = []
+salarios = []
+
+nome = input('Digite o NOME do funcionário: ')
+funcionarios.append(nome)
+salario = float(input('Informe o valor SALÁRIO mensal em reais: '))
+salarios.append(salario)
+
+repetir = input('Deseja cadastrar OUTRO funcionário? (S/N)')
+while repetir == 'S':
+    nome = input('Digite o nome do funcionário: ')
+    funcionarios.append(nome)
+    salario = float(input('Informe o valor salário mensal em reais: '))
+    salarios.append(salario)
+    repetir = input('Deseja cadastrar outro funcionário? (S/N)')
+
+print(funcionarios)
+print(salarios)
