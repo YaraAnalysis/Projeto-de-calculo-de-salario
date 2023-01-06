7cd63d635a8091a9887c785dde1d08b163464df5
YaraAnalysis <114318856+YaraAnalysis@users.noreply.github.com>
2023-01-06 17:40:09 -0300

    estrutura if para cálculo dos descontos

--- CalcularSalario.py
+++ CalcularSalario.py
@@ -10,16 +10,16 @@ salarios = []
 
 nome = input('Digite o NOME do funcionário: ')
 funcionarios.append(nome)
-salario = float(input('Informe o valor SALÁRIO mensal em reais: '))
+salario = float(input('Informe o VALOR DO SALÁRIO mensal em reais: '))
 salarios.append(salario)
 
-repetir = input('Deseja cadastrar OUTRO funcionário? (S/N)')
+repetir = input('Deseja cadastrar um funcionário? (S/N)')
 while repetir == 'S':
-    nome = input('Digite o nome do funcionário: ')
+    nome = input('Digite o NOME do funcionário: ')
     funcionarios.append(nome)
-    salario = float(input('Informe o valor salário mensal em reais: '))
+    salario = float(input('Informe o VALOR DO SALÁRIO mensal em reais: '))
     salarios.append(salario)
-    repetir = input('Deseja cadastrar outro funcionário? (S/N)')
+    repetir = input('Deseja cadastrar OUTRO funcionário? (S/N)')
 
 print(funcionarios)
 print(salarios)
@@ -28,6 +28,16 @@ qtd_funcionarios = len(funcionarios)
 print(qtd_funcionarios)
 i_funcionario = 0
 liquido = 3
+
+if salarios[i_funcionario] <= 900.0:
+    percentual = 0
+elif salario <= 1500.0:
+    percentual = 0.05
+elif salario <= 2500.0:
+    percentual = 0.1
+else:
+    percentual = 0.2
+
 while i_funcionario < qtd_funcionarios:
     print('Funcionário: {} | Salário Bruto: R$ {} | Salário Líquido: R${}.'.format(funcionarios[i_funcionario], salarios[i_funcionario], liquido))
     i_funcionario +=1
