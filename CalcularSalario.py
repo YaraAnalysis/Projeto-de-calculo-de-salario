81ea5c6235c361949717c6ad9d973f26bcd900e3
YaraAnalysis <114318856+YaraAnalysis@users.noreply.github.com>
2023-01-08 11:28:30 -0300

    Inclusão do informe sobre as taxas de desconto

--- CalcularSalario.py
+++ CalcularSalario.py
@@ -24,16 +24,17 @@ while repetir == 'S':
     else:
         ir = 0.2 * salario
     sindicato = salario * 0.03
-    fgts = salario * 0.11
     inss = salario * 0.1
     descontos = ir + inss + sindicato
     sal_liquido = salario - descontos
     liquidos.append(sal_liquido)
-    
-#print(funcionarios)
-#print(salarios)
 
 qtd_funcionarios = len(funcionarios)
+
+taxas = '\nDescontos aplicados: \n Sindicato: 3% \n INSS: 10%'
+taxas_ir = '\nBase de cálculo do IR:\n Salário Bruto até 900 (inclusive) - isento \n Salário Bruto até 1500 (inclusive) - desconto de 5% \n Salário Bruto até 2500 (inclusive) - desconto de 10% \n Salário Bruto acima de 2500 - desconto de 20%\n'
+print(taxas)
+print(taxas_ir)
 
 while i_funcionario < qtd_funcionarios:
     print('Funcionário: {} | Salário Bruto: R$ {} | Salário Líquido: R${}.'.format(funcionarios[i_funcionario], salarios[i_funcionario], liquidos[i_funcionario]))
